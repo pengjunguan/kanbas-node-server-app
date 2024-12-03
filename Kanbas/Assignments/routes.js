e9d8ad99b89031dc1b04924082b62972f747f993
Pengjun Guan <pengjunguan@Pengjuns-MacBook-Pro.local>
2024-12-02 23:31:22 -0500

    first commit

--- Kanbas/Assignments/routes.js
+++ Kanbas/Assignments/routes.js
@@ -1,33 +1,33 @@
 import * as assignmentsDao from "./dao.js";
 export default function AssignmentRoutes(app) {
-    app.put("/api/assignments/:assignmentId", (req, res) => {
+    app.put("/api/assignments/:assignmentId", async (req, res) => {
         const { assignmentId } = req.params;
         const assignmentUpdates = req.body;
-        assignmentsDao.updateAssignment(assignmentId, assignmentUpdates);
+        const status = await assignmentsDao.updateAssignment(assignmentId, assignmentUpdates);
         res.sendStatus(204);
     });
     
 
-    app.delete("/api/assignments/:assignmentId", (req, res) => {
+    app.delete("/api/assignments/:assignmentId", async (req, res) => {
         const { assignmentId } = req.params;
-        assignmentsDao.deleteAssignment(assignmentId);
+        const status = await assignmentsDao.deleteAssignment(assignmentId);
         res.sendStatus(204);
     });
 
 
-    app.post("/api/courses/:courseId/assignments", (req, res) => {
+    app.post("/api/courses/:courseId/assignments", async (req, res) => {
         const { courseId } = req.params;
         const assignment = {
         ...req.body,
         course: courseId,
         };
-        const newAssignment = assignmentsDao.createAssignment(assignment);
+        const newAssignment = await assignmentsDao.createAssignment(assignment);
         res.send(newAssignment);
     });
 
-    app.get("/api/courses/:courseId/assignments", (req, res) => {
+    app.get("/api/courses/:courseId/assignments", async (req, res) => {
         const { courseId } = req.params;
-        const assignments = assignmentsDao.findAssignmentsForCourse(courseId);
+        const assignments = await assignmentsDao.findAssignmentsForCourse(courseId);
         res.json(assignments);
     });
 
